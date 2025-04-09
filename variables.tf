544a30d1516490a930a28ceb176639da7db51396
Jha'Mel Thorne <116086991+JhamelThorne@users.noreply.github.com>
2025-04-09 19:49:30 -0400

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,36 @@
+
+variable "project" {
+  description = "Project or environment name prefix"
+  type        = string
+  default     = "3tier-app"
+}
+
+variable "region" {
+  description = "AWS region to deploy to"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDRs for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDRs for private subnets"
+  type        = list(string)
+  default     = ["10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of AZs to deploy subnets"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
