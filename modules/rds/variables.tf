fec50f57aad5e6e02d7822b4db360563d7677f09
Jha'Mel Thorne <116086991+JhamelThorne@users.noreply.github.com>
2025-04-10 21:15:07 -0400

    Update variables.tf

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,1 +1,24 @@
+variable "instance_class" {}
 
+variable "db_name" {}
+
+variable "db_username" {}
+
+variable "db_password" {
+
+  sensitive = true
+}
+variable "rds_sg_id" {}
+
+variable "private_subnet_ids" {
+
+  type = list(string)
+}
+variable "project" {}
+
+variable "publicly_accessible" {
+
+  type    = bool
+
+  default = false
+} 
