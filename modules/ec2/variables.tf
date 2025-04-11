69d3d6a19b9dfe9035bf8d88f4b719a60412f3c5
Jha'Mel Thorne <116086991+JhamelThorne@users.noreply.github.com>
2025-04-10 22:17:25 -0400

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -2,3 +2,28 @@ variable "iam_instance_profile" {
  description = "IAM instance profile to attach to EC2"
  type        = string
 }
+
+variable "ami_id" {
+ description = "AMI ID for EC2 instance"
+ type        = string
+}
+variable "instance_type" {
+ description = "EC2 instance type"
+ type        = string
+}
+variable "subnet_id" {
+ description = "Subnet ID for EC2 instance"
+ type        = string
+}
+variable "sg_id" {
+ description = "Security Group ID"
+ type        = string
+}
+variable "project" {
+ description = "Project tag"
+ type        = string
+}
+variable "iam_instance_profile" {
+ description = "IAM instance profile name"
+ type        = string
+}
