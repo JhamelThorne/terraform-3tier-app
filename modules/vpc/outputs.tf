e6e792ee74aa5644ebb8911d86f329a26f37e96e
Jha'Mel Thorne <116086991+JhamelThorne@users.noreply.github.com>
2025-04-18 14:56:52 -0400

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,14 +1,12 @@
+output "public_subnet_cidrs" {
+ value = var.public_subnet_cidrs
+ description = "List of public subnet CIDRs"
+}
+output "private_subnet_cidrs" {
+ value = var.private_subnet_cidrs
+ description = "List of private subnet CIDRs"
+}
 output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "public_subnet_ids_cidr" {
-  value = aws_subnet.public[*].cidr_block
-}
-output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
-}
-
-output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
+ value = aws_vpc.main.id
+ description = "VPC ID"
 }
