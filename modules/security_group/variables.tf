29d72ea3f90f3eac86d4988924a6878d93256eba
Jha'Mel Thorne <116086991+JhamelThorne@users.noreply.github.com>
2025-04-18 14:45:58 -0400

    Update variables.tf

--- modules/security_group/variables.tf
+++ modules/security_group/variables.tf
@@ -1,31 +1,23 @@
 variable "vpc_id" {
-
   type        = string
-
   description = "VPC ID to attach SG to"
-
 }
-
 variable "project" {
-
   type        = string
-
   description = "Project tag name"
-
 }
-
 variable "my_ip" {
-
   type        = string
-
   description = "Your public IP with CIDR mask (e.g., 98.84.x.x/32)"
-
 }
-
 variable "ec2_subnet_cidrs" {
-
   type        = list(string)
-
   description = "List of EC2 subnet CIDRs allowed to reach DB"
-
 } 
+
+variable "vpc_id" {}
+variable "my_ip" {}
+variable "project" {}
+variable "ec2_subnet_cidrs" {
+ type = list(string)
+}
