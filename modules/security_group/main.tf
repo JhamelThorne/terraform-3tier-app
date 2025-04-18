ab064c467dc1927171954f2e2365a3193e6f75b0
Jha'Mel Thorne <116086991+JhamelThorne@users.noreply.github.com>
2025-04-18 14:30:07 -0400

    Update main.tf

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -5,65 +5,35 @@ resource "aws_security_group" "app_sg" {
   description = "Allow inbound HTTP, SSH, and DB access"
 
   vpc_id      = var.vpc_id
-
   ingress {
-
     description = "HTTP from anywhere"
-
     from_port   = 80
-
     to_port     = 80
-
     protocol    = "tcp"
-
     cidr_blocks = ["0.0.0.0/0"]
-
   }
-
   ingress {
-
     description = "SSH from your IP"
-
     from_port   = 22
-
     to_port     = 22
-
     protocol    = "tcp"
-
     cidr_blocks = [var.my_ip]
-
   }
-
   ingress {
-
     description = "MySQL from EC2 subnet"
-
     from_port   = 3306
-
     to_port     = 3306
-
     protocol    = "tcp"
-
     cidr_blocks = var.ec2_subnet_cidrs
-
   }
-
   egress {
-
     from_port   = 0
-
     to_port     = 0
-
     protocol    = "-1"
-
     cidr_blocks = ["0.0.0.0/0"]
-
   }
-
   tags = {
-
     Name = "${var.project}-sg"
-
   }
 
 } 
