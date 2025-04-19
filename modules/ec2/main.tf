b20da3cbc1767652183ab38813a61f2c3fde2eb6
Jha'Mel Thorne <116086991+JhamelThorne@users.noreply.github.com>
2025-04-19 11:22:45 -0400

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,16 +1,20 @@
-resource "aws_iam_instance_profile" "ssm_profile" {
- name = "${var.project}-ssm-profile"
- role = "ec2-parameter-store"
+resource "aws_iam_instance_profile" "ec2_ssm_profile" {
+  name = "${var.project}-ssm-profile"
+  role = aws_iam_role.ec2_parameter_store.name
 }
-resource "aws_instance" "my_instance" {
- ami                         = var.ami_id
- instance_type               = var.instance_type
- subnet_id                   = var.subnet_id
- vpc_security_group_ids      = [var.sg_id]
- associate_public_ip_address = true
- iam_instance_profile = aws_iam_instance_profile.ec2_ssm_profile.name
- user_data = file("${path.module}/user_data.sh")
- tags = {
-   Name = "${var.project}-ec2"
- }
+
+resource "aws_iam_role" "ec2_parameter_store" {
+  name = "ec2-parameter-store"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
