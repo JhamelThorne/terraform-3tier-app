8b66016ccc39be78234d9347b5f068c4cb161725
Jha'Mel Thorne <116086991+JhamelThorne@users.noreply.github.com>
2025-04-21 20:22:08 -0400

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -19,3 +19,16 @@ resource "aws_iam_role" "ec2_parameter_store" {
     ]
   })
 }
+
+resource "aws_instance" "my_instance" {
+ ami                         = var.ami_id
+ instance_type               = var.instance_type
+ subnet_id                   = var.subnet_ids[0]  # Updated for better scalability
+ vpc_security_group_ids      = [var.sg_id]
+ associate_public_ip_address = true
+ iam_instance_profile        = aws_iam_instance_profile.ec2_ssm_profile.name
+ user_data                   = file("${path.module}/user_data.sh")
+ tags = {
+   Name = "${var.project}-ec2"
+ }
+}
