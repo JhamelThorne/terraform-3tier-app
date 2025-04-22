f7514f16940fc46d5fb24907d8224ee731b3b3cd
Jha'Mel Thorne <116086991+JhamelThorne@users.noreply.github.com>
2025-04-21 20:03:42 -0400

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -53,3 +53,19 @@ resource "aws_subnet" "private" {
     Name = "${var.project}-private-subnet-${count.index + 1}"
   }
 }
+
+resource "aws_route_table" "private" {
+ vpc_id = aws_vpc.main.id
+ route {
+   cidr_block     = "0.0.0.0/0"
+   nat_gateway_id = aws_nat_gateway.nat.id
+ }
+ tags = {
+   Name = "${var.project}-private-rt"
+ }
+}
+resource "aws_route_table_association" "private" {
+ count          = length(var.private_subnet_cidrs)
+ subnet_id      = aws_subnet.private[count.index].id
+ route_table_id = aws_route_table.private.id
+}
