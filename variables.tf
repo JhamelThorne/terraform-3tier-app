1dfd1ab236d1e27e79b28106ca12ebb02f265bf6
Jha'Mel Thorne <116086991+JhamelThorne@users.noreply.github.com>
2025-04-28 13:12:35 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -34,3 +34,12 @@ variable "availability_zones" {
   type        = list(string)
   default     = ["us-east-1a", "us-east-1b"]
 }
+
+variable "ami_id" {
+ description = "AMI ID for EC2 instance"
+ type        = string
+}
+variable "instance_type" {
+ description = "EC2 instance type"
+ type        = string
+}
